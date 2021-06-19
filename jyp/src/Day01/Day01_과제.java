eaf34f99d684ae6e5aeac1cf79915a9af7eb34ec
박준영 <jyp8637282@gmail.com>
2021-06-19 13:18:45 +0900

    Day01_과제

--- jyp/src/Day01/Day01_과제.java
+++ jyp/src/Day01/Day01_과제.java
@@ -1,5 +1,7 @@
 package Day01;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Day01_과제 {
 	//과제1 : 고양이 출력하기
 	/*
@@ -16,5 +18,20 @@ public class Day01_과제 {
 	 	|"^"`	 |
 	 	||_/=\\__|
 	 */
-	
+	public static void main(String[] args) {
+		//과제1 : 고양이 출력하기
+		System.out.println("\\   /\\");
+		System.out.println(" ) ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		//과제2 : 개 출력하기
+		System.out.println("|\\_/|");
+		System.out.println("|q p|\t/}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`\t |");
+		System.out.println("||_/=\\\\__|");
+	}
 }
+
+
