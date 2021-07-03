86e54a524cee8becc454d162fdf32e4bdf52ccac
박준영 <jyp8637282@gmail.com>
2021-07-03 10:48:03 +0900

    Day5

--- jyp/src/Day04/Day04_3.java
+++ jyp/src/Day04/Day04_3.java
@@ -19,51 +19,51 @@ public class Day04_3 {
 
 
 		
-		while(true) { // 무한루프
-			System.out.println(game + "번째 게임");
-			System.out.println("가위 [ 0 ] 바위 [ 1 ] 보 [ 2 ] 게임종료 [ 4 ]중 선택 : ");
-			int player = scan.nextInt();
-			if (player == 0 ) System.out.println("플레이어는 가위를 선택했습니다.");
-			else if (player == 1 ) System.out.println("플레이어는 바위를 선택했습니다.");
-			else if (player == 2 ) System.out.println("플레이어는 보를 선택했습니다.");
-			else if (player == 4 ) System.out.println("게임 종료를 선택했습니다."); break;
-			
-			
-			// 컴퓨터 => 랜덤 이용한 난수생성 [ Random : 랜덤 관련된 메소드 제공 ]
-
-			Random random = new Random();
-			int com = random.nextInt(3);
-			if (com == 0 ) System.err.println("컴퓨터는 가위를 선택했습니다.");
-			else if (com == 1 ) System.err.println("컴퓨터는 바위를 선택했습니다.");
-			else if (com == 2 ) System.err.println("컴퓨터는 보를 선택했습니다.");
-
-			//이기는 경우 수 
-			
-			if ((player == 0 && com ==2 )||(player == 1 && com == 0)||(player == 2 && com == 1)) {
-				System.out.println("플레이어가 승리했습니다.");
-				playerwin +=1;
-			}
-			else if (player == com) {
-				System.out.println("비겼습니다.");
-			}
-			else {
-				System.out.println("컴퓨터가 승리했습니다");
-				comwin +=1;
-			}
-			System.out.println("현재 스코어 \n플레이어\t컴퓨터\n  "+playerwin+"\t  "+comwin);
-			
-			game +=1;
-			
-				
-			
-			
-		} // w end
-
-		System.out.println("-------게임종료-------");
-		System.out.println("플레이한 게임 횟수 : " + game);
-		if (comwin == playerwin)System.out.println("최종 결과는 무승부입니다.");
-		else if (comwin>playerwin)System.out.println("최종 승리자는 컴퓨터입니다.");
-		else System.out.println("최종 승리자는 플레이어입니다.");
+//		while(true) { // 무한루프
+//			System.out.println(game + "번째 게임");
+//			System.out.println("가위 [ 0 ] 바위 [ 1 ] 보 [ 2 ] 게임종료 [ 4 ]중 선택 : ");
+//			int player = scan.nextInt();
+//			if (player == 0 ) System.out.println("플레이어는 가위를 선택했습니다.");
+//			else if (player == 1 ) System.out.println("플레이어는 바위를 선택했습니다.");
+//			else if (player == 2 ) System.out.println("플레이어는 보를 선택했습니다.");
+//			else if (player == 4 ) System.out.println("게임 종료를 선택했습니다."); break;
+//			
+//			
+//			// 컴퓨터 => 랜덤 이용한 난수생성 [ Random : 랜덤 관련된 메소드 제공 ]
+//
+//			Random random = new Random();
+//			int com = random.nextInt(3);
+//			if (com == 0 ) System.err.println("컴퓨터는 가위를 선택했습니다.");
+//			else if (com == 1 ) System.err.println("컴퓨터는 바위를 선택했습니다.");
+//			else if (com == 2 ) System.err.println("컴퓨터는 보를 선택했습니다.");
+//
+//			//이기는 경우 수 
+//			
+//			if ((player == 0 && com ==2 )||(player == 1 && com == 0)||(player == 2 && com == 1)) {
+//				System.out.println("플레이어가 승리했습니다.");
+//				playerwin +=1;
+//			}
+//			else if (player == com) {
+//				System.out.println("비겼습니다.");
+//			}
+//			else {
+//				System.out.println("컴퓨터가 승리했습니다");
+//				comwin +=1;
+//			}
+//			System.out.println("현재 스코어 \n플레이어\t컴퓨터\n  "+playerwin+"\t  "+comwin);
+//			
+//			game +=1;
+//			
+//				
+//			
+//			
+//		} // w end
+//
+//		System.out.println("-------게임종료-------");
+//		System.out.println("플레이한 게임 횟수 : " + game);
+//		if (comwin == playerwin)System.out.println("최종 결과는 무승부입니다.");
+//		else if (comwin>playerwin)System.out.println("최종 승리자는 컴퓨터입니다.");
+//		else System.out.println("최종 승리자는 플레이어입니다.");
 		
 	}
 	
--- jyp/src/Day04/Day04_과제.java
+++ jyp/src/Day04/Day04_과제.java
@@ -1,5 +1,45 @@
 package Day04;
 
 public class Day04_과제 {
+	public static void main(String[] args) {
+		
+		
+		
+		// 과제 1
 	
+		for(int i = 1; i <6; i++) {
+			for (int j = 0;  j<i ;j++) {
+				System.out.print("*");
+			}
+			for (int j = 0; j<10-2*i;j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0;  j<i ;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 4; i > 0 ; i --) {
+			for (int j = 0;  j<i ;j++) {
+				System.out.print("*");
+			}
+			for (int j = 0; j<10-2*i;j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0;  j<i ;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		// 과제 2
+		
+		
+		
+		
+	}
 }
