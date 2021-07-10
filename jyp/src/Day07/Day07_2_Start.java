6ff6ab55c0cd9bd47e6cc9390acbda22d1b621cd
박준영 <jyp8637282@gmail.com>
2021-07-10 13:25:14 +0900

    Day07

--- jyp/src/Day07/Day07_2_Start.java
+++ jyp/src/Day07/Day07_2_Start.java
@@ -1,82 +1,95 @@
 package Day07;
 
-import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Day07_2_Start {
-
-	// 1. 다른 클래스에서 공통적인 메모리 => static
-		// 1. scanner
-	public static Scanner scan = new Scanner(System.in);
-		// 2. 리스트나 배열
-	public static ArrayList<Member> member = new ArrayList<>();
-		// ArrayList : 리스트 클래스
-			//<제네릭> : 리스트객체에 담을 객체의 클래스
-				// 리스트명.add(객체명) : 리스트에 객체 담기
-				// 리스트명.size() : 리스트내 객체 수
 	
-				// 리스트명.get(인덱스 번호) : 리스트내 인덱스 번호에 해당하는 객체 호출
+	// 1. 다른 클래스서 공통적인 메모리 => static
+		// 1.Scanner
+	public static Scanner sc = new Scanner(System.in);
+		// 2.리스트나 배열
+	public static ArrayList<Member> members = new ArrayList<>();
+		// ArrayList :리스트 클래스
+			// <제네릭> : 리스트객체에 담을 객체의 클래스 
+				// 리스트명.add ( 객체명 ) : 리스트에 객체 담기 
 	public static void main(String[] args) {
 		
 		프로그램시작();
-	}
+		
+		
+	} // main 끝
 	
 	// 프로그램 동작 메소드 
 	public static void 프로그램시작() {
 		
-		// 파일에 존재하는 모든 회원을 리스트로 저장
+		// 파일에 존재하는 모든 회원을 리스트로 저장 
 		try {
 			FileInputStream fileInputStream = new FileInputStream("c:/java/memberlist.txt");
-			byte [] 바이트 = new byte [10000]; // 객체 하나당 100바이트
+			byte[] 바이트 = new byte[10000]; // 객체 하나당 100바이트 정도
+			
 			fileInputStream.read(바이트);
-			String 모든회원 = new String (바이트); // 바이트 => 문자열
-			// 회원별 구분
-			String [] 회원 = 모든회원.split("\n");
-					// 문자열.split("기호") : 해당 기준으로 분리
-			// 회원 마다 회원정보별 구분
-			for (int i = 0; i<회원.length-1 ; i++) {
-				
-				String[]정보 = 회원[i].split(",");
-				Member member = new Member(정보[0], 정보[1], 정보[2], Integer.parseInt(정보[3]), 정보[4], Integer.parseInt(정보[5]), Integer.parseInt(정보[6]), 정보[7]);
-					// 문자열 String => 정수형 int
+			String 모든회원 = new String(바이트); // 바이트 =>문자열
+			// 회원별 구분 
+			String[] 회원 =  모든회원.split("\n");
+					// 문자열.split("기호") : 해당 기호를 기준으로 자른다 
+			//회원마다 회원정보별 구분 
+			for(int i =0;i<회원.length-1;i++) {
+				String[] 정보 = 회원[i].split(",");
+				Member member = new Member(정보[0],정보[1],정보[2],Integer.parseInt(정보[3]),정보[4],
+						Integer.parseInt(정보[5]),Integer.parseInt(정보[6]),정보[7]);
+					// 문자열 String => 정수형 int 
 						// Integer.parseInt(문자열)
+				members.add(member); // 해당멤버를 리스트에 담기 
+			}
+		
+			
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 		// static : 정적변수 : 프로그램 시작시 메모리할당되고 모든 프로그램 종료시 메모리 초기화
-			// 단점 : 메모리 효율 감소 [ 모든 클래스에 사용되는 메모리 선언 ]
+			// 단점 : 메모리의 효율감소 [ 모든 클래스에 사용되는 메모리 선언 ] 
 		
 		while(true) {
-			System.out.println("---- 회원제 커뮤니티 ----");
-			System.out.println("1. 로그인 2. 회원가입 3. 아이디찾기 4. 비밀번호찾기 5. 프로그램종료");
-			System.out.print("---- 선택 : "); int 선택 = scan.nextInt();
+			
+			System.out.println("[[[[ 회원제 커뮤니티 ]]]]");
+			System.out.println("[[[ 1.로그인 2.회원가입 3.아이디 찾기 4. 비밀번호 찾기 5. 프로그램 종료 ]]]");
+			System.out.print("[[[선택 : "); 
+			int 선택 = sc.nextInt();
 			// 멤버 임시 객체
 			Member member = new Member();
-			if ( 선택 == 1 ) {
+			if(선택 == 1) {
 				
 			}
-			else if ( 선택 == 2 ) {
+			else if(선택 == 2) {
 				member.회원가입();
 			}
-			else if ( 선택 == 3 ) {
+			else if(선택 == 3) {
 				
 			}
-			else if ( 선택 == 4 ) {
-	
+			else if(선택 == 4) {
+				
+				
 			}
-			else if ( 선택 == 5 ) {
-				System.out.println("[종료] 이용해주셔서 감사합니다.");
+			else if (선택 == 5) {
+				System.err.println("\n [종료]  이용해주셔서 감사합니다.");
 				break;
 			}
 			else {
-				System.out.println("[경고] 알수 없는 번호입니다.");
+				System.err.println("알수 없는 번호 입니다.");
 			}
 		}
 	}
+		
+		
+		
+		
+		
+		
+		
 }
+
--- jyp/src/Day07/Member.java
+++ jyp/src/Day07/Member.java
@@ -4,117 +4,134 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 public class Member {
-
 	
-	// 필드
+	// 필드[상태]
 	String 아이디;
 	String 비밀번호;
 	String 이름;
 	int 성별;
 	String 이메일;
-	int 전화번호;
+	int 휴대전화;
 	int 포인트;
 	String 등급;
 	
 	// 생성자
-	// 1. 빈생성자
-	public Member() {
-	}
-	// 2. 전체 필드를 갖는 생성자
-		// 마우스 우클릭 => source => 생성자 혹은 메소드 등 자동완성
-	public Member(String 아이디, String 비밀번호, String 이름, int 성별, String 이메일, int 전화번호, int 포인트, String 등급) {
+	// 1. 빈생성자 
+	public Member() {}
+	// 2.전체 필드를 갖는 생성자 
+		// 마우스 오른쪽클릭 => source => 생성자 혹은 메소드등 자동완성 
+	public Member(String 아이디, String 비밀번호, String 이름, int 성별, String 이메일, int 휴대전화, int 포인트, String 등급) {
 		this.아이디 = 아이디;
 		this.비밀번호 = 비밀번호;
 		this.이름 = 이름;
 		this.성별 = 성별;
 		this.이메일 = 이메일;
-		this.전화번호 = 전화번호;
+		this.휴대전화 = 휴대전화;
 		this.포인트 = 포인트;
 		this.등급 = 등급;
 	}
-	// 3. 회원가입시 필수로 들어가는 생성자
-	public Member(String 아이디, String 비밀번호, String 이름, int 성별, int 전화번호) {
+	// 3. 회원가입 필수로 들어가는 생성자 
+	public Member(String 아이디, String 비밀번호, String 이름, int 성별, int 휴대전화) {
 		this.아이디 = 아이디;
 		this.비밀번호 = 비밀번호;
 		this.이름 = 이름;
 		this.성별 = 성별;
-		this.전화번호 = 전화번호;
+		this.휴대전화 = 휴대전화;
 		this.포인트 = 0;
 		this.등급 = "일반";
 	}
-	
-	
-	// 메소드 [ 행동 ]
-	// 1. 회원가입 메소드
+	//메소드 
+	// 1. 회원가입 메소드 
 	public void 회원가입() {
-		
-		System.out.println("---- 회원가입 화면 ----");
-		System.out.print("아이디 : "); String 아이디 = Day07_2_Start.scan.next();
-			//아이디 중복 체크
-		for (int i = 0; i < Day07_2_Start.member.size();i++) {
-			Member temp = Day07_2_Start.member.get(i);
-			if (temp.아이디.equals(아이디)) {
-				System.err.println("[경고] 동일한 아이디가 존재합니다.");
-				return; // 메소드 종료 : 메소드 반환 : 변환 데이터 없음
+		// 입력받기 
+		System.out.println("[[[ 회원가입 화면 ]]]");
+		System.out.print("아이디 : "); String 아이디 = Day07_2_Start.sc.next();
+			// 아이디 중복체크
+		for(int i = 0;i<Day07_2_Start.members.size();i++) {
+			
+			Member temp = Day07_2_Start.members.get(i);
+			// i번째 회원을 temp에 임시저장
+			if(temp.아이디.equals(아이디)) {
+				System.err.println("[경고] : 동일한 아이디가 존재합니다. [뒤로가기]");
+				return; // 메소드 종료  : 메소드 반환 : 반환데이터 없음 
 			}
 		}
-		System.out.print("비밀번호 : "); String 비밀번호 = Day07_2_Start.scan.next();
-		System.out.print("이름 : "); String 이름 = Day07_2_Start.scan.next();
-		System.out.print("성별 [남-1 / 여-2 / 미설정-0] : "); int 성별 = Day07_2_Start.scan.nextInt();
-		System.out.print("휴대전화 [- 제외] : "); int 휴대전화 = Day07_2_Start.scan.nextInt();
+		System.out.print("비밀번호 : ");String 비밀번호 = Day07_2_Start.sc.next();
+		System.out.print("이름 : ");String 이름 = Day07_2_Start.sc.next();
+		System.out.print("성별 : [남자: 1 여자: 2 미설정: 0] ");int 성별 = Day07_2_Start.sc.nextInt();
+		System.out.print("휴대전화 [ - 제외 ]: ");int 휴대전화 = Day07_2_Start.sc.nextInt();
 		
-		// 객체 만들기
+		// 객체 만들기 
 		Member member = new Member(아이디, 비밀번호, 이름, 성별, 휴대전화);
-
-		// 리스트 혹은 배열에 저장
-		Day07_2_Start.member.add(member);
-		
-		// 회원가입 성공
-		System.out.println("[성공] 회원가입에 성공했습니다.");
+		// 리스트 혹은 배열 저장 
+		Day07_2_Start.members.add(member);
+		// 회원가입 성공 
+		System.err.println("[회원가입 성공] ");
+		// 파일처리 
 		try {
 			FileOutputStream fileOutputStream = new FileOutputStream("c:/java/memberlist.txt");
-			// 리스트내 모든 회원을 파일에 저장
-			// 리스트의 for문 활용
-			for (Member temp : Day07_2_Start.member) {
-				// for ( 임시 객체명 : 리스트명 ) : 리스트내 모든 객체를 임시객체에 하나씩 대입
-				// 회원정보를 하나의 문자열로 변환 [ 필드구분 , // 회원[객체] 구분 \n
-				String 회원정보 = temp.아이디+","+temp.비밀번호+","+temp.이름+","+
-				temp.성별+","+temp.이메일+","+temp.전화번호+","+temp.포인트+","+
-				temp.등급+"\n";
+			// 리스트 내 모든 회원을 파일에 저장 
+			// 리스트의 for문 활용 
+			for(Member temp : Day07_2_Start.members) {
+				// for(임시 객체명 : 리스트명) :리스트내 모든 객체를 임시객체에 하나씩 대입 
+				// 회원정보를 하나의 문자열 변환 
+				String 회원정보 = temp.아이디+","+temp.비밀번호+","+temp.이름+
+						","+temp.성별+","+temp.이메일+","+temp.포인트+","
+						+temp.휴대전화+","+temp.등급+"\n";
 				fileOutputStream.write(회원정보.getBytes());
 			}
+			
+			
 		} catch (Exception e) {
-			System.out.println("올바르지 않은 값입니다.");
+			System.out.println("파일경로가 존재하지 않습니다.");
 		}
+		
+		
 	}
-	// 2. 로그인 메소드
+	// 2. 로그인 메소드 
 	public void 로그인() {
 		
 	}
-	// 3. 아이디찾기 메소드
+	// 3. 아이디 찾기 메소드 
 	public void 아이디찾기() {
 		
 	}
-	// 4. 비밀번호 찾기 메소드
+	// 4. 비밀번호 찾기 메소드 
 	public void 비밀번호찾기() {
 		
 	}
-	// 5. 포인트 증가 메소드
+	// 5. 포인트 증가 메소드 
 	public void 포인트증가() {
 		
 	}
-	// 6. 등급 증가 메소드
+	// 6. 등급 증가 메소드 
 	public void 등급증가() {
 		
 	}
-	// 7. 회원 탈퇴 메소드
+	// 7. 회원탈퇴 메소드 
 	public void 회원탈퇴() {
 		
 	}
-	// 8. 회원 수정 메소드
+	// 8. 회원수정 메소드
 	public void 회원수정() {
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
