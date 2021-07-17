d653db353e208a8c469f687c7f86e1f2dc7f3d13
박준영 <jyp8637282@gmai.com>
2021-07-17 11:29:26 +0900

    Day09

---
+++ jyp/src/Day09/키오스크.java
@@ -0,0 +1,16 @@
+package Day09;
+
+public interface 키오스크 {
+
+	// 필드
+	
+	// 추상메소드
+	public void 메뉴();
+	public void 장바구니();
+	public void 결제();
+	
+	// 디폴드메소드
+	public default void 종료() {
+		System.out.println("키오스크 종료");
+	}
+}
